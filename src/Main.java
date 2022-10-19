a94bf33dfb45bdecb3418aa274e158b9f972c4e7
Ante Katalinić <ante.katalinic1@gmail.com>
2022-10-19 16:24:38 +0200

    Polished hangman v6.2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         PlayerMenu playerMenu = new PlayerMenu();
-        Game game = new Game ();
+        Game game = new Game();
 
         game.initializeGame(playerMenu.playerMenu());
         System.out.println("THIS IS JUST FOR TESTING:" + playerMenu.getRandomGeneratedWord());
--- src/Words.java
+++ src/Words.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Words {
 
 
@@ -17,6 +20,49 @@ public class Words {
     private String [] animals = {"dog","cat","cow","dolphin","platypus","lion","tiger","bear","monkey","shark","kangaroo",
             "mouse","pigeon","moose"};
 
+    private int maxLength;
+    private int minLength;
+
+    public int getMaxLength() {
+        return maxLength;
+    }
+
+    public void setMaxLength(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    public int getMinLength() {
+        return minLength;
+    }
+
+    public void setMinLength(int minLength) {
+        this.minLength = minLength;
+    }
+
+    public String[] getRandomWords() {
+        return randomWords;
+    }
+
+    public void setRandomWords(String[] randomWords) {
+        this.randomWords = randomWords;
+    }
+
+    public String[] getRandomCountry() {
+        return randomCountry;
+    }
+
+    public void setRandomCountry(String[] randomCountry) {
+        this.randomCountry = randomCountry;
+    }
+
+    public String[] getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(String[] animals) {
+        this.animals = animals;
+    }
+
     private String randomGeneratedWord;
 
     public String getRandomGeneratedWord() {
@@ -29,33 +75,151 @@ public class Words {
 
     public String pickAWord (String userChoice){
         boolean checkLogic = true;
+        Scanner scanner = new Scanner(System.in);
+        int wordsLength;
 
         do {
             switch (userChoice){
 
                 case "1":
 
-                    String generateWord = randomWords[(int) (Math.random() * (randomWords.length))];
+                    randomWordsLength();
+                    System.out.println("The words in this category have minimum of " + getMinLength() + " letters," +
+                            "and maximum of " + getMaxLength() + " letters");
+
+                    System.out.print("Please choose a number of letters you wish for your word to have: ");
+
+                    wordsLength = scanner.nextInt();
+                  while (wordsLength < getMinLength() || wordsLength > getMaxLength()){
+                        System.out.println("You entered wrong number, please enter number in range of: " + minLength +"-"
+                                + maxLength);
+                        wordsLength = scanner.nextInt();
+                    }
+
+                  String generateWord = randomWords[(int) (Math.random() * (randomWords.length))];
+
+                  while (generateWord.length() != wordsLength){
+                         generateWord = randomWords[(int) (Math.random() * (randomWords.length))];
+                  }
+
                     setRandomGeneratedWord(generateWord);
                     checkLogic = false;
                     break;
 
                 case "2":
 
+                    countryWordsLength();
+
+                    System.out.println("The words in this category have minimum of " + getMinLength() + " letters," +
+                            "and maximum of " + getMaxLength() + " letters");
+
+                    System.out.print("Please choose a number of letters you wish for your word to have: ");
+
+                    wordsLength = scanner.nextInt();
+                    while (wordsLength < getMinLength() || wordsLength > getMaxLength()){
+                        System.out.println("You entered wrong number, please enter number in range of: " + minLength +"-"
+                                + maxLength);
+                        wordsLength = scanner.nextInt();
+                    }
+
+
                     String generateWordCountry = randomCountry[(int) (Math.random() * (randomCountry.length))];
+
+                    while( generateWordCountry.length() != wordsLength){
+                        generateWordCountry = randomCountry[(int) (Math.random() * (randomCountry.length))];
+                    }
+
                     setRandomGeneratedWord(generateWordCountry);
                     checkLogic = false;
                     break;
 
                 case "3":
 
+                    animalWordsLength();
+
+                    System.out.println("The words in this category have minimum of " + getMinLength() + " letters," +
+                            "and maximum of " + getMaxLength() + " letters");
+
+                    System.out.print("Please choose a number of letters you wish for your word to have: ");
+
+                    wordsLength = scanner.nextInt();
+
+                    while (wordsLength < getMinLength() || wordsLength > getMaxLength()){
+                        System.out.println("You entered wrong number, please enter number in range of: " + minLength +"-"
+                                + maxLength);
+                        wordsLength = scanner.nextInt();
+
+                    }
+
                     String generateWordAnimal = animals[(int) (Math.random() * (animals.length))];
+
+                    while(generateWordAnimal.length() != wordsLength){
+                        generateWordAnimal = animals[(int) (Math.random() * (animals.length))];
+                    }
+
                     setRandomGeneratedWord(generateWordAnimal);
                     checkLogic = false;
                     break;
             }
         } while (checkLogic);
         return getRandomGeneratedWord();
+    }
+
+    public void randomWordsLength (){
+
+        String[] lengthOfWords = getRandomWords();
+
+        int maxLength = Integer.MIN_VALUE;
+        int minLength = Integer.MAX_VALUE;
+
+        for (String word : lengthOfWords){
+            if (word.length() > maxLength){
+                maxLength = word.length();
+            }
+            if (word.length()< minLength){
+                minLength = word.length();
+            }
+        }
+        setMaxLength(maxLength);
+        setMinLength(minLength);
+    }
+
+    public void animalWordsLength (){
+
+        String[] lengthOfWords = getAnimals();
+
+        int maxLength = Integer.MIN_VALUE;
+        int minLength = Integer.MAX_VALUE;
+
+        for (String word : lengthOfWords){
+            if (word.length() > maxLength){
+                maxLength = word.length();
+            }
+            if (word.length()< minLength){
+                minLength = word.length();
+            }
+        }
+        setMaxLength(maxLength);
+        setMinLength(minLength);
+    }
+
+    public void countryWordsLength (){
+
+        String[] lengthOfWords = getRandomCountry();
+
+        int maxLength = Integer.MIN_VALUE;
+        int minLength = Integer.MAX_VALUE;
+
+        for (String word : lengthOfWords){
+            if (word.length() > maxLength){
+                maxLength = word.length();
+            }
+            if (word.length()< minLength){
+                minLength = word.length();
+            }
+        }
+        setMaxLength(maxLength);
+        setMinLength(minLength);
     }
 
 }
